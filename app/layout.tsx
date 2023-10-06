e0085356adf670eb705257b39e7b147f7f03527e
Hassan Zaib <hassanzaib97@gmail.com>
2023-10-06 11:36:20 +0500

    resolve some errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : 
 export const metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: SITE_NAME!,
+    default: SITE_NAME,
     template: `%s | ${SITE_NAME}`
   },
   robots: {
